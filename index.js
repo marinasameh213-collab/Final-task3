ef9e02f530d56e4529ecbf15c6600749d1a41ad9
Marina Sameh <marinasameh213@gmail.com>
2026-05-24 20:43:45 +0300

    upload project

---
+++ index.js
@@ -0,0 +1,33 @@
+"use strict"
+function showGame1(){
+    alert("🎮 xploring: Assassin's Creed\n Genre: Acion🔥");
+}
+
+function showGame2(){
+    alert("🎮 xploring: Cyberpunk 2077\n Genre: Open World🔥");
+}
+
+function showGame3(){
+    alert("🎮 xploring: God of War\n Genre: Adventure🔥");
+}
+
+function showGame4(){
+    alert("🎮 xploring: Elden Ring\nGenre: Fantasy🔥");
+}
+
+function showGame5(){
+    alert("🎮 xploring:Halo Infinite \nGenre: Shooter🔥");
+}
+
+function showGame6(){
+    alert("🎮 xploring:The Witcher3 \nGenre: RPG🔥");
+}
+
+function Viewall1(){
+    alert("🚀 Loading all Most Played games… (Feature coming soon!)")
+}
+
+function Viewall2(){
+    alert("🚀 Loading all Most Played games… (Feature coming soon!)")
+}
+
